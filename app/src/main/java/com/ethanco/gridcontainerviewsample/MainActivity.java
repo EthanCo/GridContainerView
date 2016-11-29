a24919210d1da3c85d3a8ff3eca9b003f19f3eda
EthanCo <fenghenyu@163.com>
2016-11-29 13:40:39 +0800

    update

--- app/src/main/java/com/ethanco/gridcontainerviewsample/MainActivity.java
+++ app/src/main/java/com/ethanco/gridcontainerviewsample/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final GridContainerView gridContainerView = (GridContainerView) findViewById(R.id.grid_container_view);
+        //gridContainerView.setSpanCount(3);
+        //gridContainerView.setSpanHeight(100); //dp
         attachNewSpan(gridContainerView);
         Button btnAddNewSpan = (Button) findViewById(R.id.btn_add_new_span);
         btnAddNewSpan.setOnClickListener(new View.OnClickListener() {
